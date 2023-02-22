a62fc9662913e62399d8f5cb28ee86b7c0937a56
Daniel Wilk <wilkdaniel03@gmail.com>
2023-02-22 17:29:45 +0100

    Configure treesitter in nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,6 +47,23 @@ end
 require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
   use "rose-pine/neovim"
+  use "nvim-treesitter/nvim-treesitter"
 end)
 
 vim.cmd("colorscheme rose-pine")
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the four listed parsers should always be installed)
+  ensure_installed = { "c" },
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+  },
+}
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "/home/dev/.local/share/nvim/site/pack/packer/start/neovim",
     url = "https://github.com/rose-pine/neovim"
   },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/home/dev/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/dev/.local/share/nvim/site/pack/packer/start/packer.nvim",
